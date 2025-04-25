1e72073d60ffa0c08cd77f65b266f1ed0017c074
Vadosmik <liontv05@gmail.com>
2025-04-25 21:56:54 +0200

    1

--- main.py
+++ main.py
@@ -261,6 +261,7 @@ def callback_handler(call):
 def message_handler(message):
   chat_id = message.chat.id
   user_id = vadim_id
+  state = user_state.get(chat_id)
 
   bot.send_message(chat_id, f"state: {state}")
 
